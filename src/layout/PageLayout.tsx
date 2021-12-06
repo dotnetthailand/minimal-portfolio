197a56bbc0ce7a7958dbfe59249fe7a79f3457b9
Thada Wangthammang <mildronize@gmail.com>
2021-12-06 16:08:01 +0700

    makue github commit graph same color

--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -8,13 +8,13 @@ import config from "../../data/SiteConfig";
 import CenterContainer from "../components/CenterContainer";
 
 import GitHubCalendar from 'github-calendar';
-import "github-calendar/dist/github-calendar-responsive.css"
+import "../themes/github-calendar-responsive.css";
 
 const PageLayout = (props: any) => {
     const { children } = props;
 
-    useEffect(()=> {
-        GitHubCalendar(".calendar", "mildronize" );
+    useEffect(() => {
+        GitHubCalendar(".calendar", "mildronize", { responsive: true });
     }, []);
 
     return (
@@ -24,13 +24,11 @@ const PageLayout = (props: any) => {
             <div>
                 <FlexContainer>
                     <MainContainer>
-                    {children}
+                        {children}
                     </MainContainer>
-                    <div>
-                    <div className="calendar">
-                        Loading the data just for you.
-                    </div>
-                    </div>
+                    <GitHubCalendarContainer>
+                      <div className="calendar" />
+                    </GitHubCalendarContainer>
                 </FlexContainer>
             </div>
             <Footer config={config} />
@@ -41,10 +39,31 @@ const PageLayout = (props: any) => {
 
 export const FlexContainer = styled.div`
   display: flex; /* or inline-flex */
+  justify-content: space-around;
 `;
 
+export const GitHubCalendarContainer = styled.div`
+
+  .calendar{
+    width: 600px;
+    .graph-before-activity-overview{
+    padding: 13px;
+    }
+    .contrib-number{
+      font-size: 18px;
+      color: inherit;
+    }
+    .float-right.color-fg-muted{
+      font-size: 10px; 
+      padding-bottom: 10px;
+    }
+  }
+
+`;
+
+
 export const MainContainer = styled.div`
-  width: 60%;
+  /* width: 100%; */
 `;
 
 export default PageLayout;
